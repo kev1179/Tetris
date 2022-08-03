9dea78ef3660b72956d073124cfc1a4f8bb71b82
Kevin <kevschool1179@gmail.com>
2022-08-02 21:39:44 -0400

    Practiced loading tetris pieces from the sprite sheet. Then practiced rotating pieces via user input and also started coming up with a timing system for how the pieces will move down the board.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
 #include "RNG.h"
+#include <vector>
 
 using namespace std;
 using namespace sf;
@@ -8,22 +9,65 @@ using namespace sf;
 int main()
 {
     RNG rng;
+    vector<vector<bool>> grid;
+    RenderWindow window(VideoMode(800, 800), "Tetris");
+    Texture gameArt;
+    gameArt.loadFromFile("sprites.png");
 
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    Sprite TPiece;
+    TPiece.setTexture(gameArt);
+    TPiece.setTextureRect(sf::IntRect(202, 688, 32, 32));
+    TPiece.setScale(5,5);
+    TPiece.setPosition(400, 0);
 
+    bool xPressed = false;
+    bool zPressed = false;
+
+    Clock clock;
     while (window.isOpen())
     {
-        sf::Event event;
+        Time time1 = clock.getElapsedTime();
+
+        Event event;
         while (window.pollEvent(event))
         {
-            if (event.type == sf::Event::Closed)
+            if (event.type == Event::Closed)
                 window.close();
         }
 
+        if (time1.asMilliseconds()  >= 80 && time1.asMilliseconds() < 1000)
+        {
+            TPiece.setPosition(TPiece.getPosition().x, TPiece.getPosition().y + 9);
+            clock.restart();
+        }
+        
+
+        
+        //*********************** USER INPUT *************************************88
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::X) && !xPressed)
+        {
+
+            TPiece.setRotation(TPiece.getRotation() + 90);
+            xPressed = Keyboard::isKeyPressed(sf::Keyboard::X);
+
+        }
+        
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Z) && !zPressed)
+        {
+
+            TPiece.setRotation(TPiece.getRotation() - 90);
+            zPressed = Keyboard::isKeyPressed(sf::Keyboard::Z);
+
+        }
+        xPressed = Keyboard::isKeyPressed(sf::Keyboard::X);
+        zPressed = Keyboard::isKeyPressed(sf::Keyboard::Z);
+
+        //*********************************** DRAWING *************************************************************
         window.clear();
-        window.draw(shape);
+
+        window.draw(TPiece);
+
         window.display();
     }
 
